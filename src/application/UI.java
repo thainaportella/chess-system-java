b322d6c15cf8726a86e63923fba2d2973c942422
Thainá Portella <thainaportellaheringer@gmail.com>
2023-06-27 14:02:26 -0300

    Trocando de jogador a cada turno

--- src/application/UI.java
+++ src/application/UI.java
@@ -3,6 +3,7 @@ package application;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
+import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.ChessPosition;
 import chess.Color;
@@ -35,6 +36,25 @@ public class UI {
 		System.out.flush();
 	}
 	
+	public static ChessPosition readChessPosition(Scanner sc) {
+		try {
+			String s = sc.nextLine();
+			char column = s.charAt(0);
+			int row = Integer.parseInt(s.substring(1));
+			return new ChessPosition(column, row);
+		}
+		catch (RuntimeException e) {
+			throw new InputMismatchException("Error reading ChessPosition. Valid values are from a1 to h8");
+		}
+	}
+	
+	public static void printMatch(ChessMatch chessMatch) {
+		printBoard(chessMatch.getPieces());
+		System.out.println();
+		System.out.println("Turn : " + chessMatch.getTurn());
+		System.out.println("Waiting player: " + chessMatch.getCurrentPlayer());
+	}
+	
 	public static void printBoard(ChessPiece[][] pieces) {
 		for (int i=0;i<pieces.length; i++) {
 			System.out.print((8 - i) + " ");
@@ -57,18 +77,6 @@ public class UI {
 		System.out.println("  a b c d e f g h");
 	}
 	
-	public static ChessPosition readChessPosition(Scanner sc) {
-		try {
-			String s = sc.nextLine();
-			char column = s.charAt(0);
-			int row = Integer.parseInt(s.substring(1));
-			return new ChessPosition(column, row);
-		}
-		catch (RuntimeException e) {
-			throw new InputMismatchException("Error reading ChessPosition. Valid values are from a1 to h8");
-		}
-	}
-	
 	private static void printPiece(ChessPiece piece, boolean background) {
     	if (background) {
     		System.out.print(ANSI_GREEN_BACKGROUND);
@@ -85,5 +93,5 @@ public class UI {
             }
         }
         System.out.print(" ");
-	}
+	}	
 }
