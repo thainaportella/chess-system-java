d100f7950db0dd6473876fd98f8319f1a4e08c7d
Thainá Portella <thainaportellaheringer@gmail.com>
2023-06-28 12:29:08 -0300

    Peão

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -33,7 +33,7 @@ public abstract class ChessPiece extends Piece {
 		return ChessPosition.fromPosition(position);
 	}
 	
-	protected boolean isThereOponentPiece (Position position) {
+	protected boolean isThereOpponentPiece (Position position) {
 		ChessPiece p = (ChessPiece)getBoard().piece(position);
 		return p != null && p.getColor() != color;
 	}
